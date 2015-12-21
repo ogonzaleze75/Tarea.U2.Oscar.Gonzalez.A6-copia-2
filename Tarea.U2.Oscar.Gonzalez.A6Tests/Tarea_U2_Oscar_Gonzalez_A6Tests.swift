ee813ebf44aa66e5e1b953442c350f32d834ce41
Oscar Gonzalez <ogonzaleze@inacap.cl>
2015-12-21 16:00:30 -0300

    Initial Commit

---
+++ Tarea.U2.Oscar.Gonzalez.A6Tests/Tarea_U2_Oscar_Gonzalez_A6Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Tarea_U2_Oscar_Gonzalez_A6Tests.swift
+//  Tarea.U2.Oscar.Gonzalez.A6Tests
+//
+//  Created by Oscar on 21-12-15.
+//  Copyright © 2015 Oscar. All rights reserved.
+//
+
+import XCTest
+@testable import Tarea_U2_Oscar_Gonzalez_A6
+
+class Tarea_U2_Oscar_Gonzalez_A6Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
